5cb10b46b4732bca9a91b84368727c923ed5cd61
subash kc <subaazkc@gmail.com>
2022-06-21 09:40:21 +0545

    refactor and osmital class added

---
+++ osmital/lib/model/osmital_model.dart
@@ -0,0 +1,49 @@
+class Osmital {
+  String? name;
+  String? address;
+  String? phone;
+  String? email;
+  String? website;
+  String? description;
+  // String? image;
+  String? lat;
+  String? lng;
+
+  Osmital({
+    this.name,
+    this.address,
+    this.phone,
+    this.email,
+    this.website,
+    this.description,
+    //  this.image,
+    this.lat,
+    this.lng,
+  });
+
+  Osmital.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    address = json['address'];
+    phone = json['phone'];
+    email = json['email'];
+    website = json['website'];
+    description = json['description'];
+    // image = json['image'];
+    lat = json['lat'];
+    lng = json['lng'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    data['address'] = address;
+    data['phone'] = phone;
+    data['email'] = email;
+    data['website'] = website;
+    data['description'] = description;
+    // data['image'] = this.image;
+    data['lat'] = lat;
+    data['lng'] = lng;
+    return data;
+  }
+}
