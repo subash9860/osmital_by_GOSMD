d927fafccb20cbeaea50c841a3175c30ca21c72a
subash kc <subaazkc@gmail.com>
2022-06-23 22:28:35 +0545

    theme added and fix splash

--- osmital/lib/main.dart
+++ osmital/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:osmital/screen/emergency_screen.dart';
 import 'package:osmital/screen/home_screen.dart';
 import 'package:osmital/screen/test_screen.dart';
 import 'package:osmital/screen/upload_image_screen.dart';
+import 'package:osmital/theme.dart';
 import 'dart:async';
 import 'package:provider/provider.dart';
 import 'provider/osmital_data_provider.dart';
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         home: const MyHomePage(),
+        theme: OsmitalTheme.light(),
         debugShowCheckedModeBanner: false,
         routes: {
           "/home": (context) => const HomeScreen(),
@@ -61,8 +63,35 @@ class SplashScreenState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        color: Colors.green,
-        child: Image.asset("Assets/Images/hospita.png"),
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+                height: 200,
+                width: 200,
+                child: Image.asset("Assets/Images/hospital.png")),
+            RichText(
+              text: const TextSpan(
+                text: "OSM",
+                style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF0061FF)),
+                children: [
+                  TextSpan(
+                    text: "ital",
+                    style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
